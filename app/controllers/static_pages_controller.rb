2204525b19e57b7044b8644b794614be7fbc32cb
russell <you@example.com>
2015-01-28 16:37:17 -0800

    basic user CRUD

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
+    
+    @russell = {name: "russell", ocupation: "developer"}
   end
 
   def help
   end
+
+  def about
+  end
+
+  def contact
+  end
+
 end
